42be5fe32c12fcb5ada740f7e5a0eeeb85c185f7
Juan Ramirez <juanmartin1111@hotmail.com>
2020-09-29 00:14:52 -0500

    Hello Wolrd

---
+++ hello-world.vscode/hello.py
@@ -0,0 +1,8 @@
+#Name: Juan Ramirez
+#Class: Software Engineering 
+#Professor: Eric Pogue
+
+msg1 = "Hello" #Variable holding Hello 
+msg2 = " World" #Second variable holding world
+hw = msg1 + msg2 #variable holding and adding the previous msg1 and msg 2 variables
+print(hw) #print statement
